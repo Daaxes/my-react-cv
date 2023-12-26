31b2c7ecdbf713c1d65a40500904c26cc6c5e389
Daaxes <daaxes@gmail.com>
2023-12-26 20:30:55 +0100

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import './App.scss';
 
 import React from 'react';
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/layout';
